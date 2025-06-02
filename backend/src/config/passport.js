082ea8a98a5826444ddf838c23ddad14cd352ead
AnisMERABTENE <anismerabtene06@gmail.com>
2025-06-02 05:24:09 +0200

    fixe problee passport

--- backend/src/config/passport.js
+++ backend/src/config/passport.js
@@ -17,8 +17,7 @@ if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/api/auth/google/callback"
-}, async (accessToken, refreshToken, profile, done) => {
+  callbackURL: "https://mobile-app-backend-production-5d60.up.railway.app/api/auth/google/callback"}, async (accessToken, refreshToken, profile, done) => {
   try {
     console.log('📧 Connexion Google tentée pour:', profile.emails[0].value);
     
